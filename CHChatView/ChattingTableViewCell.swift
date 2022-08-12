8c7bd24bb7f3d99ce3947a8dc5b89e3803fd7202
Chae Haram <djvemfla2@gmail.com>
2022-08-12 23:20:43 +0900

    tableview code설정

---
+++ CHChatView/ChattingTableViewCell.swift
@@ -0,0 +1,80 @@
+//
+//  ChattingTableViewCell.swift
+//  CHChatView
+//
+//  Created by Chae_Haram on 2022/08/12.
+//
+
+import UIKit
+
+class ChattingTableViewCell: UITableViewCell {
+    
+    let verticalStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.distribution = .fill
+        stackView.spacing = 10
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.setContentHuggingPriority(.init(rawValue: 250), for: .horizontal)
+        stackView.setContentHuggingPriority(.init(rawValue: 250), for: .vertical)
+        return stackView
+    }()
+    
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "이름"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.setContentHuggingPriority(.init(rawValue: 250), for: .horizontal)
+        label.setContentHuggingPriority(.init(rawValue: 250), for: .vertical)
+        return label
+    }()
+    
+    let statusLabel: UILabel = {
+        let label = UILabel()
+        label.text = "자기소개"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.setContentHuggingPriority(.init(rawValue: 251), for: .horizontal)
+        label.setContentHuggingPriority(.init(rawValue: 251), for: .vertical)
+        return label
+    }()
+    
+    let profilePhoto: UIImageView = {
+        let image = UIImageView()
+        image.image = UIImage(systemName: "folder.fill")
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.setContentHuggingPriority(.init(rawValue: 250), for: .horizontal)
+        image.setContentHuggingPriority(.init(rawValue: 250), for: .vertical)
+        return image
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: .default , reuseIdentifier: "ProfileCell")
+        setConstraints()
+        addSubView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func addSubView() {
+        contentView.addSubview(profilePhoto)
+        contentView.addSubview(verticalStackView)
+        verticalStackView.addArrangedSubview(nameLabel)
+        verticalStackView.addArrangedSubview(statusLabel)
+        
+    }
+    
+    func setConstraints() {
+        profilePhoto.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 15).isActive = true
+        profilePhoto.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10).isActive = true
+        profilePhoto.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -10).isActive = true
+        profilePhoto.widthAnchor.constraint(equalToConstant: 64).isActive = true
+        profilePhoto.heightAnchor.constraint(equalToConstant: 64).isActive = true
+        verticalStackView.leadingAnchor.constraint(equalTo: profilePhoto.trailingAnchor, constant: 15).isActive = true
+        verticalStackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: 15).isActive = true
+        verticalStackView.centerYAnchor.constraint(equalTo: profilePhoto.centerYAnchor).isActive = true
+        
+    }
+
+}
---
+++ CHChatView/ViewController.swift
@@ -0,0 +1,67 @@
+//
+//  ViewController.swift
+//  CHChatView
+//
+//  Created by Chae_Haram on 2022/08/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let chattingTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+
+    var randInt: Int = Int.random(in: 1...50)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print(randInt)
+        chattingTableView.delegate = self
+        chattingTableView.dataSource = self
+        chattingTableView.register(ChattingTableViewCell.self, forCellReuseIdentifier: "ProfileCell")
+        
+        addSubView()
+        setConstraints()
+        
+        title = "친구"
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    func addSubView() {
+        view.addSubview(chattingTableView)
+    }
+
+    func setConstraints() {
+        let safeArea = view.safeAreaLayoutGuide
+        
+        // tableView
+        chattingTableView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor).isActive = true
+        chattingTableView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor).isActive = true
+        chattingTableView.topAnchor.constraint(equalTo: safeArea.topAnchor).isActive = true
+        chattingTableView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor).isActive = true
+    }
+
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ProfileCell", for: indexPath) as? ChattingTableViewCell else { return UITableViewCell() }
+
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return randInt
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
+    }
+}
