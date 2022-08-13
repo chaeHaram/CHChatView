a7876085d0f61f403e267119fcf0a473e163c9a0
Chae Haram <djvemfla2@gmail.com>
2022-08-14 03:23:28 +0900

    상메 디코더블

---
+++ CHChatView/RandomStatus.swift
@@ -0,0 +1,16 @@
+//
+//  RandomStatus.swift
+//  CHChatView
+//
+//  Created by Chae_Haram on 2022/08/14.
+//
+
+import UIKit
+
+struct adviceList: Decodable {
+    let advice: String
+}
+
+struct RandomStatus: Decodable {
+    let slip: [adviceList]
+}
