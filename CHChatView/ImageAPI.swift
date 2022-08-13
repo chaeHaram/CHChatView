4408bd9c3323aa8f5e4acd53315d97842e3188cc
Chae Haram <djvemfla2@gmail.com>
2022-08-14 03:22:15 +0900

    이미지 api 호출

---
+++ CHChatView/ImageAPI.swift
@@ -0,0 +1,34 @@
+//
+//  ImageApi.swift
+//  CHChatView
+//
+//  Created by Chae_Haram on 2022/08/14.
+//
+
+import Foundation
+
+struct ImageAPI {
+//    let imageURL: URL = URL(string: "https://picsum.photos/200")!
+    let imageURL: URL = URL(string: "https://picsum.photos/v2/list")!
+    
+    func downloadImage(completion: @escaping (RandomImage?, String) -> Void ) {
+        URLSession.shared.dataTask(with: imageURL) { data, response, error in
+            guard let response = response as? HTTPURLResponse else { return }
+            guard response.statusCode == 200 else {
+                completion(nil, "데이터 통신 실패 : \(response.statusCode)")
+                return
+            }
+            
+            guard let data = data else { return }
+            
+            do {
+                let decoder = JSONDecoder()
+                let imageDown = try decoder.decode([RandomImage].self, from: data)
+                completion(imageDown[0], "데이터 통신 완료")
+            } catch {
+                completion(nil, "decode 실패")
+            }
+        }.resume()
+    }
+    
+}
