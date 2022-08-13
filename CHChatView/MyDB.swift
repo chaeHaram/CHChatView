d4a7922ad7e41c289befb81ecf67e502f7cad1ef
Chae Haram <djvemfla2@gmail.com>
2022-08-14 03:23:46 +0900

    디비 설정

---
+++ CHChatView/MyDB.swift
@@ -0,0 +1,15 @@
+//
+//  MyDB.swift
+//  CHChatView
+//
+//  Created by Chae_Haram on 2022/08/14.
+//
+
+import Foundation
+
+struct MyDB {
+    static var imageList: [RandomImage] = []
+    static var nameList: [RandomName] = []
+    static var adviceList: [adviceList] = []
+    
+}
